7c05678ea8f292053fad4a959df520236ac300cd
saiseetharam19@gmail.com <saiseetharam19@gmail.com>
2025-06-14 09:07:11 +0530

    update something

---
+++ d1.py
@@ -0,0 +1,26 @@
+print("welcome to institute")
+
+a=5
+print(id(a))
+
+a=7
+print(id(a))
+
+x = 5
+print(type(x))
+
+l=[1,2,3,4,5,"hi"]
+print(l[3])
+
+t=(1,2,3,4,"hlo")
+print(t[4])
+
+d={"name":"sai"}
+print(d)
+
+s={1,2,34,5} #
+s.add(6)
+print(s)
+
+fs=frozenset({"sai","ram"})
+print(fs)
